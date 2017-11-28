51a6d6b28bd245404312f58c370c2833cad3db97
brett weisberg <weisberg.brett@gmail.com>
2017-11-28 23:18:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Write your code in this file!
 function scuberGreetingForFeet(distance){
   if (distance <= 400){
     return 'This one is on me!'
@@ -10,17 +9,29 @@ function scuberGreetingForFeet(distance){
 }
 
 function ternaryCheckCity(city){
-return (city ==== "NYC") ? "Ok, sounds good.": "No go."
- }
-
+  // if (city === "NYC"){
+  //   return "Ok, sounds good."
+  // } else {
+  //   return "No go."
+  // }
+  return (city === "NYC") ? "Ok, sounds good." : "No go."
+}
 
 function switchOnCharmFromTip(tipString){
+  // if (tipString === "generous"){
+  //   return "Thank you so much."
+  // } else if (tipString === "not as generous"){
+  //   return "Thank you."
+  // } else if (tipString === "thanks for everything"){
+  //   return "Bye."
+  // }
+
   switch(tipString){
-  case "generous":
-  return "Thank you so much."
-  case "not as generous":
-    return "Thank you."
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
     case "thanks for everything":
-    return "Bye."
+      return "Bye."
   }
 }
