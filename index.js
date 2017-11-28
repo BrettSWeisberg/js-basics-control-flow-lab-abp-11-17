627a58d4c767bcf15a811744e99ddb2a39eece9f
brett weisberg <weisberg.brett@gmail.com>
2017-11-28 23:18:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,22 +9,11 @@ function scuberGreetingForFeet(distance){
 }
 
 function ternaryCheckCity(city){
-  // if (city === "NYC"){
-  //   return "Ok, sounds good."
-  // } else {
-  //   return "No go."
-  // }
+
   return (city === "NYC") ? "Ok, sounds good." : "No go."
 }
 
 function switchOnCharmFromTip(tipString){
-  // if (tipString === "generous"){
-  //   return "Thank you so much."
-  // } else if (tipString === "not as generous"){
-  //   return "Thank you."
-  // } else if (tipString === "thanks for everything"){
-  //   return "Bye."
-  // }
 
   switch(tipString){
     case "generous":
